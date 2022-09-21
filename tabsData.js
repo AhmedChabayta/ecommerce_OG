3fc9afb022a1fdd4204f49bdfbe7fb0161969f77
AhmedChabayta <chabays@gmail.com>
2022-09-21 17:42:59 +0300

    Initial commit from Create Next App

---
+++ tabsData.js
@@ -0,0 +1,52 @@
+const tabsData = [
+  {
+    name: 'Clothing & Shoes',
+    icon: 'hanger',
+  },
+  {
+    name: 'Entertainment',
+    icon: 'film',
+  },
+  {
+    name: 'Music',
+    icon: 'music',
+  },
+  {
+    name: 'Sports & Lifestyle',
+    icon: 'dumbell',
+  },
+  {
+    name: 'Pets',
+    icon: 'bone',
+  },
+  {
+    name: 'Kitchen Accessories',
+    icon: 'fork',
+  },
+  {
+    name: 'Travel Equipment',
+    icon: 'binoculars',
+  },
+  {
+    name: 'Gardening',
+    icon: 'leaf',
+  },
+  {
+    name: 'Tools',
+    icon: 'bolt',
+  },
+  {
+    name: 'Mothers Love',
+    icon: 'crib',
+  },
+  {
+    name: 'Toys & Games',
+    icon: 'hazard',
+  },
+  {
+    name: 'Classic',
+    icon: 'wheel',
+  },
+];
+
+export default tabsData;
